9b4b1a498bc6e90c368e7db0a9573ec456b79ebc
Laura Erskine <lrskne@gmail.com>
2013-07-29 10:25:26 -0700

    Only the rails new at this point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySymptoms::Application.config.secret_token = '0371b3ee70b0764c1b951d4efb7dfbfbb2498bd8c10eb1a08b61236940359c1edb2ac28afe429df456725bacff19bfddbb8b8608a53a3f22211acf53dd522a4f'
